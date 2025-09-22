e7369d7b98ba75c534f05178bf267702ee74e4a3
MatiOrtegga <groundwtf911@gmail.com>
2025-09-22 16:06:58 -0300

    Generate commands,queries and handlers for the application, and add mediaTr package to the proyect.

---
+++ ConexaTest.Application/Queries/Movies/GetMovieByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexaTest.Application.Queries.Movies
+{
+    internal class GetMovieByIdQuery
+    {
+    }
+}
